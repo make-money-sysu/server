c54fc2a1b70aef6392c2ff74ae3570b53952fa39
ltstriker <ltuse163@163.com>
2019-05-13 14:43:14 +0800

    增加站内消息文档

---
+++ controllers/msg.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"server/models"
+	"time"
+
+	"github.com/astaxie/beego"
+	"github.com/bitly/go-simplejson"
+)
+
+// PackageController operations for Package
+type msgController struct {
+	beego.Controller
+}
+
+func (this *msgController) Post() {
+	
+}
+
+func (this *msgController) Put() {
+
+}
+
+func (this *msgController) Get() {
+
+}
